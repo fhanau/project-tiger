f625af73e17b791c21424188ca4d012b0ac18fe2
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-09 09:25:48 -0400

    Add crow server code and required dependencies

---
+++ src/client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "crow.h"
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "crow.h"
+
+int main(int argc, char** argv) {
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/")([]() {
+    return "Welcome to Project Tiger";
+  })
+
+  app.port(18080).multithreaded().run();
+}
