af2a2109ac4c646f041d08f1f6ae128578f1f994
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-11 00:08:44 -0400

    Add routing to create host account

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,11 +4,28 @@
 
 #include "crow.h"
 
+using namespace std;
+
 int main(int argc, char** argv) {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")([]() {
     return "Welcome to Project Tiger";
+  });
+
+  CROW_ROUTE(app, "/create/<string>/<string>")([](string username, string password) {
+    // This is where SQLWrapper.usernameExists() would be called
+    if (true) {
+      return crow::response("Error: Username is already taken");
+    }
+
+    int isSuccessful = 1; // This is where the Authenicator.create() method is called
+    
+    if (isSuccessful) {
+      return crow::response("Account creation successful");
+    } else {
+      return crow::response("Account creation unsuccessful. Try again.");
+    }
   });
 
   app.port(18080).multithreaded().run();
