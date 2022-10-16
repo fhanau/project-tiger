1ee2b78241751fbb59121dbc1e450357291550fa
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-16 13:38:27 -0400

    Remove namespace std and add sessionId for clients

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,16 +4,21 @@
 
 #include "crow.h"
 
-using namespace std;
-
 int main(int argc, char** argv) {
   crow::SimpleApp app;
+
+  // Create a unique session id based on the app object's location in memory
+  // Give this to clients as proof they logged in successfully
+  uint32_t id = reinterpret_cast<uint32_t>(&app);
+  char literal[10];
+  sprintf(literal, "%I64u", id);
+  std::string session(literal);
 
   CROW_ROUTE(app, "/")([]() {
     return "Welcome to Project Tiger";
   });
 
-  CROW_ROUTE(app, "/create/<string>/<string>")([](string username, string password) {
+  CROW_ROUTE(app, "/create/<string>/<string>")([](std::string username, std::string password) {
     // This is where SQLWrapper.usernameExists() would be called
     if (true) {
       return crow::response("Error: Username is already taken");
@@ -23,13 +28,13 @@ int main(int argc, char** argv) {
     int isSuccessful = 1;
     
     if (isSuccessful) {
-      return crow::response("Account creation successful");
+      return crow::response(session);
     } else {
-      return crow::response("Account creation unsuccessful. Try again.");
+      return crow::response("");
     }
   });
 
-  CROW_ROUTE(app, "/login/<string>/<string>")([] (string username, string password) {
+  CROW_ROUTE(app, "/login/<string>/<string>")([] (std::string username, std::string password) {
     // This is where SQLWrapper.isValidHost() would be called
     // Would need to encrypt password with Authenticator.encrypt() first
     int isValidLogin = 1;
@@ -37,9 +42,9 @@ int main(int argc, char** argv) {
       // Could also return a session token to client as proof of successful login
       // Token would be required to access private information
       // Token could be randomly generated everytime the server is started and stored as a global variable
-      return crow::response("Login successful.");
+      return crow::response(session);
     } else {
-      return crow::response("Login unsuccessful. Try again.");
+      return crow::response("");
     }
   });
 
