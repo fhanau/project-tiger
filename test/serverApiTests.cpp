c7ec373c0cb2e0a19e43cf7d6c2e594eb41909b6
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-24 13:04:32 -0400

    Remove database tests

--- test/serverApiTests.cpp
+++ test/serverApiTests.cpp
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
 #include "../src/server/util.h"
-#include "../src/sqliteDB/sql.h"
 
 TEST(ServerUtilTest, ReturnsUniqueSession) {
   std::string sessionId = getSession();
@@ -15,16 +14,4 @@ TEST(ServerUtilTest, ReturnsSameUniqueSession) {
   std::string sessionIdFirstCall = getSession();
   std::string sessionIdSecondCall = getSession();
   ASSERT_EQ(sessionIdFirstCall, sessionIdSecondCall);
-}
-
-TEST(ServerUtilTest, ReturnsSameDatabase) {
-  Database databaseFirstCall = getDatabase();
-  Database databaseSecondCall = getDatabase();
-  char firstLocation[9];
-  snprintf(firstLocation, sizeof(firstLocation), "%p", &databaseFirstCall);
-  std::string firstMemoryLocationLiteral = std::string(firstLocation);
-  char secondLocation[9];
-  snprintf(secondLocation, sizeof(secondLocation), "%p", &databaseSecondCall);
-  std::string secondMemoryLocationLiteral = std::string(secondLocation);
-  ASSERT_EQ(firstMemoryLocationLiteral, secondMemoryLocationLiteral);
 }
