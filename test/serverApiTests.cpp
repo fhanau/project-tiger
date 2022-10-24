933acd86a746655bfd6c3c85f60497d61d70f03a
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-24 09:13:49 -0400

    parent b64e1552451e79c8ffcdd62521c3275feb8cfeec author Michael Lawes mjl2243 <mjalawes@gmail.com> 1666617229 -0400 committer Michael Lawes mjl2243 <mjalawes@gmail.com> 1666648786 -0400
    parent b64e1552451e79c8ffcdd62521c3275feb8cfeec
    author Michael Lawes mjl2243 <mjalawes@gmail.com> 1666617229 -0400
    committer Michael Lawes mjl2243 <mjalawes@gmail.com> 1666648680 -0400
    
    Add database check for login and create on server and client side
    
    Test server util functions
    
    Test server util functions
    
    Change test to check for pointer as string
    
    Not working
    
    Create stat.cpp
    
    create stat file

--- test/serverApiTests.cpp
+++ test/serverApiTests.cpp
@@ -1,5 +1,30 @@
 #include "gtest/gtest.h"
+#include "../src/server/util.h"
+#include "../src/sqliteDB/sql.h"
 
-TEST(ServerApiTest0, HandlesEmptyInput) {
-  EXPECT_EQ(1, 1);
+TEST(ServerUtilTest, ReturnsUniqueSession) {
+  std::string sessionId = getSession();
+  EXPECT_GE(sessionId.size(), 1);
+
+  std::string firstTwoCharsOfSessionId = sessionId.substr(0, 2);
+  std::string memoryLocationPointerPrefix = "0x";
+  ASSERT_EQ(firstTwoCharsOfSessionId, memoryLocationPointerPrefix);
+}
+
+TEST(ServerUtilTest, ReturnsSameUniqueSession) {
+  std::string sessionIdFirstCall = getSession();
+  std::string sessionIdSecondCall = getSession();
+  ASSERT_EQ(sessionIdFirstCall, sessionIdSecondCall);
+}
+
+TEST(ServerUtilTest, ReturnsSameDatabase) {
+  Database databaseFirstCall = getDatabase();
+  Database databaseSecondCall = getDatabase();
+  char firstLocation[9];
+  snprintf(firstLocation, sizeof(firstLocation), "%p", &databaseFirstCall);
+  std::string firstMemoryLocationLiteral = std::string(firstLocation);
+  char secondLocation[9];
+  snprintf(secondLocation, sizeof(secondLocation), "%p", &databaseSecondCall);
+  std::string secondMemoryLocationLiteral = std::string(secondLocation);
+  ASSERT_EQ(firstMemoryLocationLiteral, secondMemoryLocationLiteral);
 }
