247a46e30a87135f027b31b28f7ea437af7a9eae
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-24 09:13:49 -0400

    Test server util functions

--- test/serverApiTests.cpp
+++ test/serverApiTests.cpp
@@ -1,5 +1,26 @@
 #include "gtest/gtest.h"
+#include "../src/server/util.h"
+#include "../src/sqliteDB/sql.h"
 
-TEST(ServerApiTest0, HandlesEmptyInput) {
-  EXPECT_EQ(1, 1);
+TEST(ServerUtilTest, ReturnsUniqueSession) {
+  std::string sessionId = getSession();
+  EXPECT_GE(sessionId.size(), 1);
+
+  std::string firstTwoCharsOfSessionId = sessionId.substr(0, 2);
+  std::string memoryLocationPointerPrefix = "0x";
+  ASSERT_EQ(firstTwoCharsOfSessionId, memoryLocationPointerPrefix);
+}
+
+TEST(ServerUtilTest, ReturnsSameUniqueSession) {
+  std::string sessionIdFirstCall = getSession();
+  std::string sessionIdSecondCall = getSession();
+  ASSERT_EQ(sessionIdFirstCall, sessionIdSecondCall);
+}
+
+TEST(ServerUtilTest, ReturnsSameDatabase) {
+  Database databaseFirstCall = getDatabase();
+  Database databaseSecondCall = getDatabase();
+  Database* firstCallPointer = &databaseFirstCall;
+  Database* secondCallPointer = &databaseSecondCall;
+  ASSERT_EQ(databaseFirstCall, databaseSecondCall);
 }
