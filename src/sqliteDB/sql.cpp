f67a373033730adb4173894b1ceb30a5b5200896
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-24 22:43:13 -0400

    Change char* from constant to array

--- src/sqliteDB/sql.cpp
+++ src/sqliteDB/sql.cpp
@@ -138,7 +138,7 @@ int Database::checkExists(std::string command) {
 
 char *Database::getId(std::string command) {
     char* messageError;
-    char *id = "";
+    char id[50];
     int exit = sqlite3_open(directory, &DB);
     exit = sqlite3_exec(DB, command.c_str(), getIdCallback, id,
       &messageError);
