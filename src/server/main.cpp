e5fd4deb330e26997d883ef32b1dcfc6110afc3e
Michael Lawes mjl2243 <mjalawes@gmail.com>
2022-10-25 19:37:53 -0400

    Remove printing command to server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -89,7 +89,6 @@ int main(int argc, char** argv) {
     std::string values = firstValues + secondValues + thirdValues;
 
     std::string command = insert + values;
-    std::cout << command << "\n";
     getDatabase().insertData(command);
     return "SUCCESS";
   });
