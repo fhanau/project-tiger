4010646ff78ea919987d408ce8700d97c869c96c
Michael Lawes <mjalawes@gmail.com>
2022-11-10 19:26:09 -0500

    Update error code we check for

--- src/sqliteDB/sql.cpp
+++ src/sqliteDB/sql.cpp
@@ -281,7 +281,7 @@ std::string Database::getTextValue(std::string command) {
     int exit = sqlite3_open(directory, &DB);
     sqlite3_stmt* queryResult = makeStatement(command);
     exit = sqlite3_step(queryResult);
-    if (exit != SQLITE_OK) {
+    if (exit != SQLITE_ROW) {
         std::cerr << exit << "\n";
         std::cerr << "Error when getting text value\n";
         sqlite3_free(messageError);
