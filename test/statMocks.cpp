427198cd198c0dab3cf226be8a6cf502fbca1fff
ab5181 <ab5181@columbia.edu>
2022-11-28 14:49:00 -0500

    Remove uninteresting GMock warning using NiceMock

--- test/statMocks.cpp
+++ test/statMocks.cpp
@@ -9,16 +9,19 @@ class MockData {
  public:
     MOCK_METHOD(std::vector<int>, pulledIntDataVector,
       (Database& db, const std::string& sql_command), ());
+    MOCK_METHOD(std::vector<int>, pulledIntDataVector,
+      (), ());
 };
 
 TEST(MedianMock, gettingMockMedian) {
-  MockData theData;
+  testing::NiceMock<MockData> theData;
   Database db = Database("dummy5.db");
   std::vector<int> result {1, 2, 3, 4, 5};
 
   ON_CALL(theData, pulledIntDataVector)
     .WillByDefault(testing::Return(result));
 
-  int med = medianValue(theData.pulledIntDataVector(db, "dummyCommand"));
+  std::vector<int> test = theData.pulledIntDataVector(db, "dummyCom");
+  int med = medianValue(test);
   EXPECT_EQ(med, 3);
 }
